24fbaae8d41674827a5f0ec72d09fd7eb3bc4111
IvanArango9823MRB <ivan.arango@twowheelstogo.com>
2022-02-21 15:53:34 -0600

    Actualizacion repo docker

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ module.exports = {
     STRIPE_PUBLIC_API_KEY: appConfig.STRIPE_PUBLIC_API_KEY
   },
   webpack: (webpackConfig) => {
+    
     webpackConfig.module.rules.push({
       test: /\.(gql|graphql)$/,
       loader: "graphql-tag/loader",
